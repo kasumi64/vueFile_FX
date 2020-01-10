00524e7dff164034b9490f665389285d0239c40b
kasumi64 <77638292@qq.com>
2020-01-10 15:03:09 +0800

    修改vuecli标题

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
 			filename: 'index.html',
 			// 当使用 title 选项时，
 			// template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-			title: 'vue-cli4',
+			title: 'fbcs_配置系统',
 			// 在这个页面中包含的块，默认情况下会包含
 			// 提取出来的通用 chunk 和 vendor chunk。
 			chunks: ['chunk-vendors', 'chunk-common', 'index']
